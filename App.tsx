40d12f1301b96e4a99c0a59d975276ca2477a778
stepanturchenko <stepanstepan4@gmail.com>
2020-11-07 15:04:06 +0200

    add succsess conect

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import {StyleSheet, Text, View, Button} from 'react-native'
-import {WifiWizard, HotspotWizard} from 'react-native-wifi-and-hotspot-wizard';
+
+//@ts-ignore
+import {WifiWizard, HotspotWizard} from 'react-native-wifi-and-hotspot-wizard'
 
 export default function App() {
 
-    const connect = () => {
-
+    const connect = async () => {
+        const enable = await WifiWizard.isWifiEnabled()
+        if (!enable) {
+            return console.log('NOT WORK WIFI')
+        }
+        WifiWizard.getNearbyNetworks().then((networks: any) => {
+            alert(JSON.stringify(networks))
+            // let jsonNetworks = JSON.parse(networks)
+            // alert(jsonNetworks)
+            console.log(networks, 'ALL JSON')
+            let network = networks.filter((Network: any) => {
+                return Network.SSID == "library"
+            })
+            if (network) {
+                WifiWizard.connectToNetwork(network[0], 'library', '').then((status: any) => {
+                    console.log('RESULT', status)
+                    alert(status + 'RESULT')
+                    if (status == "connected") {
+                        // Further Tasks
+                    }
+                }).catch((err: any) => alert(err + 'ERR'))
+            }
+        })
     }
     return (
         <View style={styles.container}>
